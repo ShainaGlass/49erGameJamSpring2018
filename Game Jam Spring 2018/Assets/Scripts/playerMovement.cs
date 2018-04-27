e594986039519823577fed6825fa1696586dc8ac
ShainaGlass <32582433+ShainaGlass@users.noreply.github.com>
2018-04-27 19:50:35 -0400

    Basic Physic script
    Created three objects, floor, base, and stackable object. Created a script to drag the base across the floor object along the x-axis. The stackable object falls and sits on the base.

---
+++ Game Jam Spring 2018/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+    public Vector2 mousePos;
+    public Camera mainCamera;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        mousePos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, mousePos.y, mainCamera.nearClipPlane));
+        Vector3 temp = transform.position;
+        temp.x = mousePos.x;
+        transform.position = temp;
+	}
+}
