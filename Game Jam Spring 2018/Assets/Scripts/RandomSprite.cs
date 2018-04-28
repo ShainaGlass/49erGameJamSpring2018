0975fef408a51d5adeb062ae0fe025ccac095ffa
ShainaGlass <32582433+ShainaGlass@users.noreply.github.com>
2018-04-28 15:12:02 -0400

    Created an order menu
    Order menu is located below the base object in the middle of the floor object. It contains 6 random sprites to represent what the player should build.

---
+++ Game Jam Spring 2018/Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour {
+
+    public Sprite[] sprites;
+    int spriteIndex;
+	// Use this for initialization
+	void Start () {
+        spriteIndex = Random.Range(0, sprites.Length);
+        GetComponent<SpriteRenderer>().sprite = sprites[spriteIndex];
+     }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
