b7b6e1e0e8542d4b61c0facbc1dbdbf65c4fddb7
rrobbie72 <robertawatkins72@gmail.com>
2018-04-27 22:07:50 -0400

    Basic Random Spawn
    These scripts spawn 50 random objects with a set timer (150) that increases in speed every time an object falls for difficulty.

---
+++ Game Jam Spring 2018/Assets/Scripts/randomFallingObjects.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomFallingObjects : MonoBehaviour 
+{
+
+	// Use this for initialization
+
+	public GameObject prefab;
+	public float MAX_SPAWN_TIME = 50;
+	public float remainingTime =0;
+	public float objectsRemaining = 10;
+		// Instantiate the prefab somewhere between -10.0 and 10.0 on the x-z plane
+		void Start()
+	{
+		
+	}
+	private void FixedUpdate() 
+	{
+		if (remainingTime > 0) 
+		{
+			remainingTime--;
+		} 
+		else 
+		{
+			if (objectsRemaining > 0) 
+			{
+				Vector3 position = new Vector3 (Random.Range (-7f, 7f), Random.Range (0f, 5f),0);
+				Instantiate (prefab, position, Quaternion.identity);
+				remainingTime = MAX_SPAWN_TIME;
+				objectsRemaining--;
+
+				if (MAX_SPAWN_TIME > 50) 
+				{
+					MAX_SPAWN_TIME -= 10;
+				}
+			}
+		}
+	}
+}
---
+++ Game Jam Spring 2018/Assets/Scripts/testFall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testFall : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform prefab;
+	void Start()
+	{
+	}
+	private void fall() {
+		for (int i = 0; i < 10; i++)
+		{
+			Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+		}
+	}
+}
