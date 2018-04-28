fa04842e0455ab7e202580577a92a6b3e9a1f878
ShainaGlass <32582433+ShainaGlass@users.noreply.github.com>
2018-04-27 22:04:41 -0400

    Freeze object on contact
    Our falling object will stay on our base object even when moving.

--- Game Jam Spring 2018/Assets/Scripts/playerMovement.cs
+++ Game Jam Spring 2018/Assets/Scripts/playerMovement.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour {
     public Vector2 mousePos;
     public Camera mainCamera;
-
-
+    public Rigidbody2D rb2d;
+    public float playerSpeed = 50;
+    public float currentVelocity;
 	// Use this for initialization
 	void Start () {
-		
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         mousePos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, mousePos.y, mainCamera.nearClipPlane));
-        Vector3 temp = transform.position;
-        temp.x = mousePos.x;
-        transform.position = temp;
+        //Vector3 temp = transform.position;
+        //temp.x = mousePos.x;
+        //rb2d.MovePosition(temp);
+        Vector2 tempVelocity = rb2d.velocity;
+        tempVelocity.x = Mathf.Clamp(mousePos.x - transform.position.x, -1, 1) * playerSpeed;
+        currentVelocity = tempVelocity.x;
+        rb2d.velocity = tempVelocity;
 	}
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        GameObject other = collision.gameObject;
+        Rigidbody2D orb2d = other.GetComponent<Rigidbody2D>();
+        orb2d.velocity = rb2d.velocity;
+    }
 }
